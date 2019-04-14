b40b029ab1b1a3f95e16979a65069e6ddd58a118
Theodor René Carlsen <theodorc@stud.ntnu.no>
2019-04-14 16:29:55 +0200

    Inital commit - Local voting for either pb or sesam

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { score_pb: 0, score_sesam: 0}
+    this.handleVoteChange = this.handleVoteChange.bind(this)
+    this.handleReset = this.handleReset.bind(this)
+  }
+
+  //onclick function
+  handleVoteChange(team) {
+    //Check if user has voted
+    if (localStorage.getItem("hasVoted")){
+      return;
+    }
+    if (team === "pb") {
+      this.setState({ score_pb: this.state.score_pb + 1 });
+    } else if (team === "sesam") {
+      this.setState({ score_sesam: this.state.score_sesam + 1 });
+    }
+    localStorage.setItem("hasVoted", "True");
+  }
+
+  //onclick function
+  handleReset = () => {this.setState({score_pb:0}); this.setState({score_sesam:0})}
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Sesam vs Pizzabakeren</h1>
         </header>
+        <div class="main">
+          <Scoreboard pb={this.state.score_pb} sesam={this.state.score_sesam} />
+          <Button team="pb" verbose="Pizzabakeren" handleChange={this.handleVoteChange} />
+          <Button team="sesam" verbose="Sesam" handleChange={this.handleVoteChange} />
+          <Button team="NULL" verbose="Reset" handleChange={this.handleReset} />
+        </div>
       </div>
+    );
+  }
+}
+class Scoreboard extends Component{
+  render() {
+    return(
+      <div>
+        <h1>Pizzabakeren har {this.props.pb} </h1>
+        <h1>Sesam har {this.props.sesam} </h1>
+      </div>
+    );
+  };
+}
+
+class Button extends Component{
+
+  vote = team => {this.props.handleChange(team);};
+
+  render(){
+    return(
+      <button onClick={() => this.vote(this.props.team)}>{this.props.verbose}</button>
     );
   }
 }
