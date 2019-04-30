02982c8341ef37558efad1faebf4ab95325d3877
Theodor René Carlsen <theodorc@stud.ntnu.no>
2019-04-30 20:57:08 +0200

    add Work in progress page

--- src/index.js
+++ src/index.js
@@ -11,9 +11,10 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <Router>
-    <Navbar />
+      {window.location.pathname!=="/" && <Navbar />}
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={WorkInProgress} />
+      <Route exact path="/home" component={Home} />
       <Route exact path="/curr" component={CurriculumBooks} />
       <Route path="/edit/" component={App} />
       <Route path="/index" component={Scoreboard} />
@@ -21,6 +22,23 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
+
+function WorkInProgress(props){
+
+const center2 = {
+    position: "absolute",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%"
+}
+    return(
+        <div style={center2}>
+            <h1 className="white-text"> Work in progress </h1>
+        </div>
+    )
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
