2de4b999955b3fe5c498be4619dfb471e4c2d513
Theodor René Carlsen <theodorc@stud.ntnu.no>
2019-05-07 21:33:45 +0200

    Add yarn start for backend and fancy running server message

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const port = 8000
 const bodyParser = require('body-parser')
 const Pool = require('pg').Pool
 const cors = require('cors')
+const { exec } = require('child_process');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -29,7 +30,15 @@ const pool = new Pool({
 app.get('/', (req, res) => getAllBooks(req,res))
 //app.get('/test', (req, res) => res.json({ info: 'Node.js, Express, and Postgres API' }))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+    console.clear()
+    console.log(`
+    \n\n\n\n\n\n\n\n\n\n\n\n
+    currbooks📚 backend running on port ${port}🔥`)
+    exec('figlet CB.backend', (err,stdout,stderr) => {
+    console.log(`${stdout}`)
+    })
+})
 
 const getAllBooks = (req, res) => {
     pool.query('SELECT * FROM books', (err,results) => {
