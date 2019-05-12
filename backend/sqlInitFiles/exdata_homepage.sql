c1763f4df18a66cb80b5df85b25c89f09502b28b
theodorc <theodor.rene@gmail.com>
2019-05-12 13:41:38 +0200

    Add more exdata

--- backend/sqlInitFiles/exdata_homepage.sql
+++ backend/sqlInitFiles/exdata_homepage.sql
@@ -1,1 +1,10 @@
-INSERT INTO project (title, imp_path, description, link) VALUES ('Pleieassistent - Sykehjem', '/theodor.jpg/','Jobbet som pleieassistent ved sykehjem.', 'trondheimkommune.no');
+INSERT INTO project (title, img_path, description, link, type) 
+    VALUES 
+    ('Pleieassistent - Sykehjem', '/theodor.jpg/','Jobbet som pleieassistent ved sykehjem.', 'trondheimkommune.no','jobb');
+INSERT INTO project (title, img_path, description, link, type) 
+    VALUES 
+    ('Utikler - Tripletex', '/theodor.jpg/','Sommerjobb hos Tripletex som utvikler. Jobbet med React Native', 'tripletex.no','jobb');
+
+INSERT INTO project (title, img_path, description, link) 
+    VALUES 
+    ('Denne siden', '/theodor.jpg/','Skrev denne siden for å lære meg nodestack(react,express,postgres)', 'theodorc.no','prosjekt');
