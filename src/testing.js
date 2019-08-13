1a41d0832cdfbfebd580880c4c0a8bdce90b9ad6
Theodor René Carlsen <theodorc@samfundet.no>
2019-08-13 20:19:32 +0200

    more changes

--- src/testing.js
+++ src/testing.js
@@ -7,14 +7,19 @@ export default function TestCards(props){
         <div className="container">
             <TestCard />
             <TestCard />
+            <TestCard />
+            <TestCard />
         </div>
     )
 }
 function TestCard(props){
     return (
         <div className="cardTest" >
-            <img alt="random" src="https://picsum.photos/200/300"/>
-            <p> Hello </p>
+            <h3 className= "titleTest"> Title </h3>
+            <div className="imageContainer">
+                <img alt="random" src="https://picsum.photos/300/300"/>
+            </div>
+            <p className="textTest"> Hello </p>
         </div>
     )
 }
